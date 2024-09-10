372bae2ade0131e059d40937d96eccf376a4f05a
Olivier Djatchue <60683035+OlivierDjatchue@users.noreply.github.com>
2024-09-10 22:31:51 +0200

    Update app.py

--- APP/app.py
+++ APP/app.py
@@ -54,4 +54,5 @@ if __name__ == "__main__":
         PGADMIN_URL="https://www.youtube.com/"
 
     # Dynamically assign port
-    app.run(host="0.0.0.0", port=args.port)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
